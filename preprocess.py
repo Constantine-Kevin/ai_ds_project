0746899f1e69448558f272361727ddf6d0f2e0d8
Avatar Azka <avatar.azka@gmail.com>
2021-06-03 17:27:50 +0700

    Added Naive Bayes file

--- preprocess.py
+++ preprocess.py
@@ -13,7 +13,6 @@ stop_word.remove('not')
 #Uncomment below ONCE if the file isn't installed in computer
 #nltk.download('stopwords')
 def preprocess_string(str_arg,dictio):
-    
     #Clean the string from any HTML Tag
     cleanr = re.compile('<.*?>')
     cleaned_str =re.sub(cleanr, ' ',str_arg)
@@ -34,10 +33,12 @@ def preprocess_string(str_arg,dictio):
         dictio[i]=dictio.get(i,0)+1
     #print(dictio)
     return
+
 def main():
     a="i just saw this movie on TV..<br /><br />i've lost my dad when i was young and this movie surely did touch me..<br /><br />i can feel the lost that the little girl Desi felt..<br /><br />the feeling of wanting to see her father again..<br /><br />wanting to talk to him..<br /><br />or at least given the chance to say goodbye..<br /><br />and i'm so touched with the letter that was wrote back to her..<br /><br />saying that her father read her letter, and sent it back to someone to reply her and buy her a present because there isn't a shop in heaven..<br /><br />it just lets me feel that miracles do exist.."
     b="balbla blasd <br> ajsd! lasdda <h1>jasd"
     c="The goose just cannot doesn't, like being looked upon by person not wearing any glasses."
     print(preprocess_string(c,{}))
+
 if __name__=="__main__":
     main()
