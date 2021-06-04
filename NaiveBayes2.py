a734419b8573bec0ee6de199bbccc211aacd2ed3
1CeB3Rg <kevinbravo444@gmail.com>
2021-06-04 14:52:24 +0700

    Fix zero padding 2

--- NaiveBayes2.py
+++ NaiveBayes2.py
@@ -28,19 +28,17 @@ def naivebayes(word_list,pos_dict,neg_dict):
     p_positive=0.5
     p_negative=0.5
     #Count how many zeros will happen
-    pos_zero_to_add=0
-    neg_zero_to_add=0
+    to_add=0
     for i in word_list:
-        if pos_dict.get(i,0)==0:
-            pos_zero_to_add+=1
-        if neg_dict.get(i,0)==0:
-            neg_zero_to_add+=1
+        if pos_dict.get(i,0)==0 and neg_dict.get(i,0)==0:
+            to_add+=1
+
 
     for i in word_list:
         #P(i|positive)
-        p_positive *= (pos_dict.get(i,0)+1)/(sum(pos_dict.values()) + len(pos_dict)*(pos_zero_to_add+ neg_zero_to_add)+ pos_zero_to_add)
+        p_positive *= (pos_dict.get(i,0)+1)/(sum(pos_dict.values()) + len(pos_dict)*(to_add)+ to_add)
         #P(i|negative)
-        p_negative *= (neg_dict.get(i,0)+1)/(sum(neg_dict.values()) + len(neg_dict)*(pos_zero_to_add+ neg_zero_to_add) + neg_zero_to_add)
+        p_negative *= (neg_dict.get(i,0)+1)/(sum(neg_dict.values()) + len(neg_dict)*(to_add) + to_add)
     
     if p_positive>p_negative:
         return "positive"
