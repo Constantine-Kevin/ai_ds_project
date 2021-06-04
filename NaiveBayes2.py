5cb3cf457802907c961f7d9c644eac1167a215c2
1CeB3Rg <kevinbravo444@gmail.com>
2021-06-04 15:02:23 +0700

    Fix zero padding 3

--- NaiveBayes2.py
+++ NaiveBayes2.py
@@ -36,9 +36,9 @@ def naivebayes(word_list,pos_dict,neg_dict):
 
     for i in word_list:
         #P(i|positive)
-        p_positive *= (pos_dict.get(i,0)+1)/(sum(pos_dict.values()) + len(pos_dict)*(to_add)+ to_add)
+        p_positive *= (pos_dict.get(i,0)+to_add)/(sum(pos_dict.values()) + len(pos_dict)*(to_add)+ to_add)
         #P(i|negative)
-        p_negative *= (neg_dict.get(i,0)+1)/(sum(neg_dict.values()) + len(neg_dict)*(to_add) + to_add)
+        p_negative *= (neg_dict.get(i,0)+to_add)/(sum(neg_dict.values()) + len(neg_dict)*(to_add) + to_add)
     
     if p_positive>p_negative:
         return "positive"
